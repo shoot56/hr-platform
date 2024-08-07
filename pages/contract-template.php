ea77ea5b53cfbcbfb7e6ccd7135f998680592008
org100h <hmymerao@procoders.tech>
2024-08-07 17:55:09 +0300

    Add contract link rewrite rule and template redirect
    Implemented a new rewrite rule to handle HR contract links, adding a query variable for contract IDs. Introduced a template redirect to serve a new contract template page when the contract ID is present.

---
+++ pages/contract-template.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+</head>
+<body>
+<h1>Welcome to contract of: <?php echo esc_html($contractId ?? 0) ?></h1>
+<!-- Other Template Content -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
