b984cb6ff43a321b6f9f470f2c4b26edfec3ce1b
khymerao <khymerao@procoders.tech>
2024-09-30 12:45:31 +0300

    Refactor and reintroduce document content filtering
    Move `hr_filter_document_content` to a new position for better readability and reintroduce previously removed code. Added new filters to enhance document notifications and owner change options, bringing better integration with ACF fields.

--- hr-platform.php
+++ hr-platform.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: HR Platform
 Description: Platform for recruiters and HR
-Version: 1.0.10
+Version: 1.0.4
 Author: Shutko Dmytro
 Author URI: http://procoders.tech
 
@@ -14,7 +14,8 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
-function hr_platform_check_acf_plugin() {
+function hr_platform_check_acf_plugin()
+{
     $acf_free_active = is_plugin_active('advanced-custom-fields/acf.php');
     $acf_pro_active = is_plugin_active('advanced-custom-fields-pro/acf.php');
 
@@ -62,13 +63,6 @@ function save_pdf_on_post_save($post_id, $post, $update)
     generate_pdf_for_offer($post_id, true);
 }
 
-function hr_filter_document_content($document_id, $content, $fields)
-{
-    foreach ($fields as $key => $field) {
-        update_field($key, esc_html($field), 'dashboard_' . $document_id);
-    }
-    return $content;
-}
 
 add_filter('esignature_content', function ($document_content, $document_id) {
 
@@ -86,11 +80,86 @@ add_filter('esignature_content', function ($document_content, $document_id) {
 }, 10, 2);
 
 
+function hr_filter_document_content($document_id, $content, $fields)
+{
+    foreach ($fields as $key => $field) {
+        update_field($key, esc_html($field), 'dashboard_' . $document_id);
+    }
+    return $content;
+}
+
 add_action('esig_document_advanced_settings', function ($document_id, $data) {
     if (is_plugin_active('hr-platform/hr-platform.php') && isset($data['acf'])) {
         $data['document_content'] = hr_filter_document_content($document_id, $data['document_content'], $data['acf']);
     }
 }, 10, 2);
+
+add_filter('esig-document-notification-content', function () {
+
+    if (function_exists('acf_get_field_groups')) {
+        $field_groups = acf_get_field_groups();
+        $fields = [];
+        if (!empty($field_groups)) {
+            foreach ($field_groups as $field_group) {
+                if ($field_group['key'] === 'group_hr_contract') {
+                    $field_objects = acf_get_fields($field_group['key']);
+                    if (!empty($field_objects)) {
+                        foreach ($field_objects as $field) {
+                            $fields[$field['name']] = $field['label'];
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    $shortcodes_list = '';
+    if (!empty($fields)) {
+        foreach ($fields as $field_key => $field_label) {
+            $shortcodes_list .= '<li><input onclick="this.select()" type="text" value="{' . $field_key . '}" /> - ' . $field_label . '</li>';
+        }
+    }
+
+    $html = '<div id="shortcodediv" class="postbox">';
+    $html .= '<h3 class="hndle esig-section-title"><span>' . __('Shortcode List', 'hr-platform') . '</span></h3>';
+    $html .= '<div class="inside">';
+    $html .= '<div><ul>' . $shortcodes_list . '</ul></div>';
+    $html .= '</div></div>';
+
+    echo $html;
+
+});
+
+
+add_filter('esig_owner_change_option', function ($doc_id) {
+
+    acf_form_head();
+    if (function_exists('acf_add_options_sub_page')) {
+        acf_add_options_sub_page(array(
+            'page_title' => 'Dashboard unique options',
+            'menu_title' => 'Dashboard Options',
+            'parent_slug' => $doc_id,
+        ));
+    }
+    echo '<div id="shortcodefielddiv" class="postbox">';
+    echo  '<div class="inside">';
+    acf_form(
+        array(
+            'id' => 'acf-esign-form',
+            'post_id' => 'dashboard_' . $doc_id,
+            'new_post' => false,
+            'form' => false,
+            'instruction_placement' => 'field',
+            'field_groups' => array('group_hr_contract'),
+            'return' => admin_url('admin.php?post_type=esign'), // Redirect to same page after form is submitted
+            'html_before_fields' => '',
+            'html_after_fields' => '',
+        ));
+    echo '<style>#shortcodediv{display: none}</style>';
+    echo '</div></div>';
+});
+
 
 include_once plugin_dir_path(__FILE__) . 'includes/post-type.php';
 include_once plugin_dir_path(__FILE__) . 'includes/acf-fields.php';
